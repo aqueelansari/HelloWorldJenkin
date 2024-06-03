7bb49e9c677d42674e68477be1b56765078510ce
aqueelansari <aqueelansari1@gmail.com>
2024-06-03 20:22:37 +0530

    Initial Commit

---
+++ HelloGroovy.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+println('I am groovy')
+for(j in 0..4){ print j }
+0.upto(3) { print "$it" }
+4.times{ print "$it" }
+
+String str= 'I am here ';
+
+println str.reverse();
+
+
